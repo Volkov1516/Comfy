3cb1903dec0c6939098a977cd9247da879547432
Vlad Volkov <from5jforever@gmail.com>
2022-06-13 14:04:36 +0300

    Refactor PromoCategories component
    - Set image for slider

--- client/components/PromoCategories.tsx
+++ client/components/PromoCategories.tsx
@@ -15,9 +15,9 @@ const PromoCategories: NextPage = () => {
             <div className={css.body}>
                 <div className={css.item}>
                     <a className={css.code} href="#">Код: 1234567</a>
-
-                    <div className={css.slide}></div>
-
+                    <div className={css.slide}>
+                        <img src="/apple_iphone_11_64gb_black_0_3.webp" alt="Banner" width="180px" height="180px" />
+                    </div>
                     <div className={css.info}>
                         <div className={css.name}><a href="#">Смартфон Apple iPhone 11 64Gb Black</a></div>
                         <div className={css.feedback}>
@@ -51,24 +51,152 @@ const PromoCategories: NextPage = () => {
                     </div>
                 </div>
                 <div className={css.item}>
-                    <div className={css.code}></div>
-                    <div className={css.slide}></div>
-                    <div className={css.info}></div>
+                    <a className={css.code} href="#">Код: 1234567</a>
+                    <div className={css.slide}>
+                        <img src="/apple_iphone_11_64gb_black_0_3.webp" alt="Banner" width="180px" height="180px" />
+                    </div>
+                    <div className={css.info}>
+                        <div className={css.name}><a href="#">Смартфон Apple iPhone 11 64Gb Black</a></div>
+                        <div className={css.feedback}>
+                            <div>
+                                feedback
+                            </div>
+                            <a className={css.comments}>
+                                <img src="/chat-bubble.svg" alt="Banner" width="15px" height="15px" />
+                                <span>63</span>
+                            </a>
+                        </div>
+                        <div className={css.labels}>
+                            <img src="/labels/5m.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/abank.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/mono.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/np.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/privat.svg" alt="Banner" width="30px" height="30px" />
+                        </div>
+                        <div className={css.actions}>
+                            <div className={css.price}>
+                                <div className={css.priceCurrent}>22 999</div>
+                            </div>
+                            <div className={css.actionButton}>
+                                <img src="/cart.svg" alt="Banner" width="20px" height="20px" />
+                            </div>
+                        </div>
+                        <div className={css.bonus}>
+                            <img src="/coin.svg" alt="Banner" width="14px" height="14px" />
+                            <span><b className={css.pinted}>+200$</b> на бонусный счет</span>
+                        </div>
+                    </div>
                 </div>
                 <div className={css.item}>
-                    <div className={css.code}></div>
-                    <div className={css.slide}></div>
-                    <div className={css.info}></div>
+                    <a className={css.code} href="#">Код: 1234567</a>
+                    <div className={css.slide}>
+                        <img src="/apple_iphone_11_64gb_black_0_3.webp" alt="Banner" width="180px" height="180px" />
+                    </div>
+                    <div className={css.info}>
+                        <div className={css.name}><a href="#">Смартфон Apple iPhone 11 64Gb Black</a></div>
+                        <div className={css.feedback}>
+                            <div>
+                                feedback
+                            </div>
+                            <a className={css.comments}>
+                                <img src="/chat-bubble.svg" alt="Banner" width="15px" height="15px" />
+                                <span>63</span>
+                            </a>
+                        </div>
+                        <div className={css.labels}>
+                            <img src="/labels/5m.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/abank.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/mono.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/np.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/privat.svg" alt="Banner" width="30px" height="30px" />
+                        </div>
+                        <div className={css.actions}>
+                            <div className={css.price}>
+                                <div className={css.priceCurrent}>22 999</div>
+                            </div>
+                            <div className={css.actionButton}>
+                                <img src="/cart.svg" alt="Banner" width="20px" height="20px" />
+                            </div>
+                        </div>
+                        <div className={css.bonus}>
+                            <img src="/coin.svg" alt="Banner" width="14px" height="14px" />
+                            <span><b className={css.pinted}>+200$</b> на бонусный счет</span>
+                        </div>
+                    </div>
                 </div>
                 <div className={css.item}>
-                    <div className={css.code}></div>
-                    <div className={css.slide}></div>
-                    <div className={css.info}></div>
+                    <a className={css.code} href="#">Код: 1234567</a>
+                    <div className={css.slide}>
+                        <img src="/apple_iphone_11_64gb_black_0_3.webp" alt="Banner" width="180px" height="180px" />
+                    </div>
+                    <div className={css.info}>
+                        <div className={css.name}><a href="#">Смартфон Apple iPhone 11 64Gb Black</a></div>
+                        <div className={css.feedback}>
+                            <div>
+                                feedback
+                            </div>
+                            <a className={css.comments}>
+                                <img src="/chat-bubble.svg" alt="Banner" width="15px" height="15px" />
+                                <span>63</span>
+                            </a>
+                        </div>
+                        <div className={css.labels}>
+                            <img src="/labels/5m.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/abank.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/mono.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/np.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/privat.svg" alt="Banner" width="30px" height="30px" />
+                        </div>
+                        <div className={css.actions}>
+                            <div className={css.price}>
+                                <div className={css.priceCurrent}>22 999</div>
+                            </div>
+                            <div className={css.actionButton}>
+                                <img src="/cart.svg" alt="Banner" width="20px" height="20px" />
+                            </div>
+                        </div>
+                        <div className={css.bonus}>
+                            <img src="/coin.svg" alt="Banner" width="14px" height="14px" />
+                            <span><b className={css.pinted}>+200$</b> на бонусный счет</span>
+                        </div>
+                    </div>
                 </div>
                 <div className={css.item}>
-                    <div className={css.code}></div>
-                    <div className={css.slide}></div>
-                    <div className={css.info}></div>
+                    <a className={css.code} href="#">Код: 1234567</a>
+                    <div className={css.slide}>
+                        <img src="/apple_iphone_11_64gb_black_0_3.webp" alt="Banner" width="180px" height="180px" />
+                    </div>
+                    <div className={css.info}>
+                        <div className={css.name}><a href="#">Смартфон Apple iPhone 11 64Gb Black</a></div>
+                        <div className={css.feedback}>
+                            <div>
+                                feedback
+                            </div>
+                            <a className={css.comments}>
+                                <img src="/chat-bubble.svg" alt="Banner" width="15px" height="15px" />
+                                <span>63</span>
+                            </a>
+                        </div>
+                        <div className={css.labels}>
+                            <img src="/labels/5m.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/abank.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/mono.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/np.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/privat.svg" alt="Banner" width="30px" height="30px" />
+                        </div>
+                        <div className={css.actions}>
+                            <div className={css.price}>
+                                <div className={css.priceCurrent}>22 999</div>
+                            </div>
+                            <div className={css.actionButton}>
+                                <img src="/cart.svg" alt="Banner" width="20px" height="20px" />
+                            </div>
+                        </div>
+                        <div className={css.bonus}>
+                            <img src="/coin.svg" alt="Banner" width="14px" height="14px" />
+                            <span><b className={css.pinted}>+200$</b> на бонусный счет</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
