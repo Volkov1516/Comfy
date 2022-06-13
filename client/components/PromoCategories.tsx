5ab2628590fc1fabcf26c671dd8e96d3a288cb7c
Vlad Volkov <from5jforever@gmail.com>
2022-06-13 13:37:34 +0300

    Create PromoCategories component
    - Add layout
    - Add images

---
+++ client/components/PromoCategories.tsx
@@ -0,0 +1,78 @@
+import type { NextPage } from 'next';
+
+import css from '../styles/PromoCategories.module.scss';
+
+const PromoCategories: NextPage = () => {
+    return (
+        <section className={css.container}>
+            <div className={css.header}>
+                <h3>Скидки до -40%</h3>
+                <a className={css.more} href="#">
+                    <span>Смотреть все</span>
+                    <img src="/arrowPromo.svg" alt="Banner" width="20px" height="20px" />
+                </a>
+            </div>
+            <div className={css.body}>
+                <div className={css.item}>
+                    <a className={css.code} href="#">Код: 1234567</a>
+
+                    <div className={css.slide}></div>
+
+                    <div className={css.info}>
+                        <div className={css.name}><a href="#">Смартфон Apple iPhone 11 64Gb Black</a></div>
+                        <div className={css.feedback}>
+                            <div>
+                                feedback
+                            </div>
+                            <a className={css.comments}>
+                                <img src="/chat-bubble.svg" alt="Banner" width="15px" height="15px" />
+                                <span>63</span>
+                            </a>
+                        </div>
+                        <div className={css.labels}>
+                            <img src="/labels/5m.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/abank.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/mono.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/np.svg" alt="Banner" width="30px" height="30px" />
+                            <img src="/labels/privat.svg" alt="Banner" width="30px" height="30px" />
+                        </div>
+                        <div className={css.actions}>
+                            <div className={css.price}>
+                                <div className={css.priceCurrent}>22 999</div>
+                            </div>
+                            <div className={css.actionButton}>
+                                <img src="/cart.svg" alt="Banner" width="20px" height="20px" />
+                            </div>
+                        </div>
+                        <div className={css.bonus}>
+                            <img src="/coin.svg" alt="Banner" width="14px" height="14px" />
+                            <span><b className={css.pinted}>+200$</b> на бонусный счет</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={css.item}>
+                    <div className={css.code}></div>
+                    <div className={css.slide}></div>
+                    <div className={css.info}></div>
+                </div>
+                <div className={css.item}>
+                    <div className={css.code}></div>
+                    <div className={css.slide}></div>
+                    <div className={css.info}></div>
+                </div>
+                <div className={css.item}>
+                    <div className={css.code}></div>
+                    <div className={css.slide}></div>
+                    <div className={css.info}></div>
+                </div>
+                <div className={css.item}>
+                    <div className={css.code}></div>
+                    <div className={css.slide}></div>
+                    <div className={css.info}></div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default PromoCategories;
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -5,12 +5,14 @@ import '../styles/globals.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Slider from '../components/Slider'
+import PromoCategories from '../components/PromoCategories'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Slider />
+      <PromoCategories />
       <Component {...pageProps} />
       <Footer />
     </>
