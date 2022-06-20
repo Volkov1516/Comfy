24ac6eb08eec6d017975175500f7e18ac9a386ef
Vlad Volkov <from5jforever@gmail.com>
2022-06-20 16:23:32 +0300

    Add database connection
    - Configure MongoDB cloud
    - Configure connection to the database
    - Install mongoose
    - Install dotenv

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import router from './routes/test';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
 
 const PORT = 5000;
 
@@ -9,6 +13,11 @@ app.use(express.json());
 
 app.use('/api/v1/test', router);
 
-app.listen(PORT, () => {
-    console.log('Server is running on port 5000...');
-});
+mongoose
+    .connect(process.env.MONGODB_URI as string)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log('Server is running on port 5000...');
+        });
+    })
+    .catch((err) => console.log(err));
