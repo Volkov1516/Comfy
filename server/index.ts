db1c29a89e35c4fe672302b9c6c086bfed62a167
Vlad Volkov <from5jforever@gmail.com>
2022-06-20 12:42:53 +0300

    Create test route and controller
    - Add routes folder
    - Add controllesrs folder
    - Add .env to .the gitignore

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import router from './routes/test';
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000...')
+const PORT = 5000;
+
+app.use(express.json());
+
+app.use('/api/v1/test', router);
+
+app.listen(PORT, () => {
+    console.log('Server is running on port 5000...');
 });
