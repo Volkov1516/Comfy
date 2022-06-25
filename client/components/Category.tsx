c11283639d753accb4eb0087ea5b18b7eb85a883
Vlad Volkov <from5jforever@gmail.com>
2022-06-25 15:53:16 +0300

    Add link from category to products
    - Change npm run dev oprions

--- client/components/Category.tsx
+++ client/components/Category.tsx
@@ -20,14 +20,17 @@ const Category = () => {
     return (
         <div className={css.container}>
             <div>Breadcrumbs</div>
-
             <div className={css.category}>
                 {category?.data.map((item: { catalogId: string, categories: [] }) => (
                     <>
                         {item.categories.map((item: any) => (
                             <div className={css.categoryItem}>
-                                <img src="/img/apple4.webp" alt="Logo" width="80px" height="80px" />
-                                <h2>{item?.title}</h2>
+                                <Link href="/products">
+                                    <a>
+                                        <img src="/img/apple4.webp" alt="Logo" width="80px" height="80px" />
+                                        <h2>{item?.title}</h2>
+                                    </a>
+                                </Link>
                                 <ul>
                                     {item?.products.map((item: any) => (
                                         <li>{item}</li>
