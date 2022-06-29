c5deae947644946f7c01bb0c2f36449d3db21af9
Vlad Volkov <from5jforever@gmail.com>
2022-06-29 15:10:28 +0300

    Refactor Carousel component
    - Add static behavior
    - Change layout
    - Change styles

--- client/components/Carousel.tsx
+++ client/components/Carousel.tsx
@@ -1,48 +1,55 @@
+import { useState, useEffect } from 'react';
+
 import css from '../styles/Carousel.module.scss';
 
-interface CarouselTypes {
-    title: string;
-    titleSize: string;
-}
-
-const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const items = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const Carousel = () => {
+    const [offset, setOffset] = useState(0);
+
+    const handleNextSlide = () => {
+        if (offset === -((items.length - 4) * 100)) return;
+
+        setOffset(offset - 100);
+    };
+
+    const handlePrevSlide = () => {
+        if (offset === 0) return;
+
+        setOffset(offset + 100);
+    };
+
     return (
         <div className={css.container}>
-            <p className={css.title}>Хиты продаж</p>
+            <h3 className={css.header}>Header</h3>
             <div className={css.carousel}>
-                <div className={css.btnPrev}>
-                    <img src="/img/discArrow.svg" alt="button left" width="16px" height="16px" />
+                <div className={css.btnPrev} onClick={handlePrevSlide}>
+                    <img src="/img/discArrow.svg" alt="Arrow" width="16px" height="16px" />
                 </div>
-
-
-                <div className={css.items}>
-                    {items.map((item: any) => (
-                        <div className={css.item}>
-                            <div className={css.info}>
-                                <div className={css.img}>
-                                    <img src="/img/apple_iphone_11_64gb_black_0_3.webp" alt="Banner" />
+                <div className={css.window}>
+                    {items.map((i: any) => (
+                        <div className={css.itemContainer} style={{ transform: `translate(${offset}%)` }}>
+                            <div className={css.item}>
+                                <div className={css.image}>
+                                    <img src="/img/apple_iphone_11_64gb_black_0_3.webp" alt="Banner" width="144px" height="144px" />
                                 </div>
                                 <div className={css.text}>
-                                    <a href="#"> {item} Смартфон Apple iPhone 11 64Gb Black</a>
+                                    <a href="#">Смартфон Apple iPhone 11 64Gb Black</a>
                                 </div>
-                            </div>
-                            <div className={css.actions}>
-                                <div className={css.price}>
-                                    <div className={css.priceCurrent}>22 999</div>
-                                </div>
-                                <div className={css.actionButton}>
-                                    <img src="/img/cart.svg" alt="Banner" width="20px" height="20px" />
+                                <div className={css.actions}>
+                                    <div className={css.price}>
+                                        <div className={css.priceCurrent}>22 999</div>
+                                    </div>
+                                    <div className={css.actionButton}>
+                                        <img src="/img/cart.svg" alt="Banner" width="20px" height="20px" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     ))}
                 </div>
-
-
-                <div className={css.btnNext}>
-                    <img src="/img/discArrow.svg" alt="button right" width="16px" height="16px" />
+                <div className={css.btnNext} onClick={handleNextSlide}>
+                    <img src="/img/discArrow.svg" alt="Arrow" width="16px" height="16px" />
                 </div>
             </div>
         </div>
