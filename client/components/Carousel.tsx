0d2ffce6f0822f1a40f1229fc1006505c6661347
Vlad Volkov <from5jforever@gmail.com>
2022-06-29 15:45:46 +0300

    Add mock data for Carousel component
    - Provide moks data
    - Change border color

--- client/components/Carousel.tsx
+++ client/components/Carousel.tsx
@@ -2,10 +2,65 @@ import { useState, useEffect } from 'react';
 
 import css from '../styles/Carousel.module.scss';
 
-const items = [1, 2, 3, 4, 5, 6, 7, 8];
+type ItemTypes = {
+    img: string;
+    text: string;
+    price: number;
+}
+type CarouselTypes = {
+    header: string;
+    items: ItemTypes[];
+}
 
-const Carousel = () => {
+const mockData = {
+    header: 'Посмотреть еще',
+    items: [
+        {
+            img: "/img/apple_iphone_11_64gb_black_0_3.webp",
+            text: "Смартфон Apple iPhone 11 64Gb Black",
+            price: 22999,
+        },
+        {
+            img: "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/ua-galaxy-a53-5g-a536-sm-a536ezkhsek-531826029_1__1.jpg",
+            text: "Смартфон Samsung Galaxy A53 128Gb Black",
+            price: 15999,
+        },
+        {
+            img: "/img/apple_iphone_11_64gb_black_0_3.webp",
+            text: "Смартфон Apple iPhone 11 64Gb Black",
+            price: 22999,
+        },
+        {
+            img: "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/ua-galaxy-a53-5g-a536-sm-a536ezkhsek-531826029_1__1.jpg",
+            text: "Смартфон Samsung Galaxy A53 128Gb Black",
+            price: 15999,
+        },
+        {
+            img: "/img/apple_iphone_11_64gb_black_0_3.webp",
+            text: "Смартфон Apple iPhone 11 64Gb Black",
+            price: 22999,
+        },
+        {
+            img: "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/ua-galaxy-a53-5g-a536-sm-a536ezkhsek-531826029_1__1.jpg",
+            text: "Смартфон Samsung Galaxy A53 128Gb Black",
+            price: 15999,
+        },
+        {
+            img: "/img/apple_iphone_11_64gb_black_0_3.webp",
+            text: "Смартфон Apple iPhone 11 64Gb Black",
+            price: 22999,
+        },
+        {
+            img: "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/ua-galaxy-a53-5g-a536-sm-a536ezkhsek-531826029_1__1.jpg",
+            text: "Смартфон Samsung Galaxy A53 128Gb Black",
+            price: 15999,
+        },
+    ]
+}
+
+const Carousel = ({ header = mockData.header, items = mockData.items }: CarouselTypes) => {
     const [offset, setOffset] = useState(0);
+    console.log(mockData.header)
 
     const handleNextSlide = () => {
         if (offset === -((items.length - 4) * 100)) return;
@@ -21,27 +76,27 @@ const Carousel = () => {
 
     return (
         <div className={css.container}>
-            <h3 className={css.header}>Header</h3>
+            <h3 className={css.header}>{header}</h3>
             <div className={css.carousel}>
                 <div className={css.btnPrev} onClick={handlePrevSlide}>
-                    <img src="/img/discArrow.svg" alt="Arrow" width="16px" height="16px" />
+                    <img src="/img/discArrow.svg" alt="Button left" width="16px" height="16px" />
                 </div>
                 <div className={css.window}>
-                    {items.map((i: any) => (
+                    {mockData?.items?.map((i: any) => (
                         <div className={css.itemContainer} style={{ transform: `translate(${offset}%)` }}>
                             <div className={css.item}>
                                 <div className={css.image}>
-                                    <img src="/img/apple_iphone_11_64gb_black_0_3.webp" alt="Banner" width="144px" height="144px" />
+                                    <img src={i.img} alt="Product image" width="144px" height="144px" />
                                 </div>
                                 <div className={css.text}>
-                                    <a href="#">Смартфон Apple iPhone 11 64Gb Black</a>
+                                    <a href="#">{i.text}</a>
                                 </div>
                                 <div className={css.actions}>
                                     <div className={css.price}>
-                                        <div className={css.priceCurrent}>22 999</div>
+                                        <div className={css.priceCurrent}>{i.price}</div>
                                     </div>
                                     <div className={css.actionButton}>
-                                        <img src="/img/cart.svg" alt="Banner" width="20px" height="20px" />
+                                        <img src="/img/cart.svg" alt="Cart image" width="20px" height="20px" />
                                     </div>
                                 </div>
                             </div>
@@ -49,7 +104,7 @@ const Carousel = () => {
                     ))}
                 </div>
                 <div className={css.btnNext} onClick={handleNextSlide}>
-                    <img src="/img/discArrow.svg" alt="Arrow" width="16px" height="16px" />
+                    <img src="/img/discArrow.svg" alt="Button right" width="16px" height="16px" />
                 </div>
             </div>
         </div>
