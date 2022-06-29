990ec8b636159f322f1a54a082df6ee2db340a23
Vlad Volkov <from5jforever@gmail.com>
2022-06-29 16:00:27 +0300

    Create mock folder
    - Add mockCarouselData

--- client/components/Carousel.tsx
+++ client/components/Carousel.tsx
@@ -1,66 +1,26 @@
 import { useState, useEffect } from 'react';
+import cx from 'classnames';
 
 import css from '../styles/Carousel.module.scss';
+
+import { mockCarouselData } from '../mocks/mockCarouselData';
+
+type Display = 'normal' | 'small';
 
 type ItemTypes = {
     img: string;
     text: string;
     price: number;
 }
+
 type CarouselTypes = {
+    display: Display;
     header: string;
     items: ItemTypes[];
 }
 
-const mockData = {
-    header: 'Посмотреть еще',
-    items: [
-        {
-            img: "/img/apple_iphone_11_64gb_black_0_3.webp",
-            text: "Смартфон Apple iPhone 11 64Gb Black",
-            price: 22999,
-        },
-        {
-            img: "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/ua-galaxy-a53-5g-a536-sm-a536ezkhsek-531826029_1__1.jpg",
-            text: "Смартфон Samsung Galaxy A53 128Gb Black",
-            price: 15999,
-        },
-        {
-            img: "/img/apple_iphone_11_64gb_black_0_3.webp",
-            text: "Смартфон Apple iPhone 11 64Gb Black",
-            price: 22999,
-        },
-        {
-            img: "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/ua-galaxy-a53-5g-a536-sm-a536ezkhsek-531826029_1__1.jpg",
-            text: "Смартфон Samsung Galaxy A53 128Gb Black",
-            price: 15999,
-        },
-        {
-            img: "/img/apple_iphone_11_64gb_black_0_3.webp",
-            text: "Смартфон Apple iPhone 11 64Gb Black",
-            price: 22999,
-        },
-        {
-            img: "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/ua-galaxy-a53-5g-a536-sm-a536ezkhsek-531826029_1__1.jpg",
-            text: "Смартфон Samsung Galaxy A53 128Gb Black",
-            price: 15999,
-        },
-        {
-            img: "/img/apple_iphone_11_64gb_black_0_3.webp",
-            text: "Смартфон Apple iPhone 11 64Gb Black",
-            price: 22999,
-        },
-        {
-            img: "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/ua-galaxy-a53-5g-a536-sm-a536ezkhsek-531826029_1__1.jpg",
-            text: "Смартфон Samsung Galaxy A53 128Gb Black",
-            price: 15999,
-        },
-    ]
-}
-
-const Carousel = ({ header = mockData.header, items = mockData.items }: CarouselTypes) => {
+const Carousel = ({ display = 'normal', header = mockCarouselData.header, items = mockCarouselData.items }: CarouselTypes) => {
     const [offset, setOffset] = useState(0);
-    console.log(mockData.header)
 
     const handleNextSlide = () => {
         if (offset === -((items.length - 4) * 100)) return;
@@ -82,7 +42,7 @@ const Carousel = ({ header = mockData.header, items = mockData.items }: Carousel
                     <img src="/img/discArrow.svg" alt="Button left" width="16px" height="16px" />
                 </div>
                 <div className={css.window}>
-                    {mockData?.items?.map((i: any) => (
+                    {items?.map((i: any) => (
                         <div className={css.itemContainer} style={{ transform: `translate(${offset}%)` }}>
                             <div className={css.item}>
                                 <div className={css.image}>
---
+++ client/mocks/mockCarouselData.ts
@@ -0,0 +1,45 @@
+export const mockCarouselData = {
+    header: 'Посмотреть еще',
+    items: [
+        {
+            img: "/img/apple_iphone_11_64gb_black_0_3.webp",
+            text: "Смартфон Apple iPhone 11 64Gb Black",
+            price: 22999,
+        },
+        {
+            img: "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/ua-galaxy-a53-5g-a536-sm-a536ezkhsek-531826029_1__1.jpg",
+            text: "Смартфон Samsung Galaxy A53 128Gb Black",
+            price: 15999,
+        },
+        {
+            img: "/img/apple_iphone_11_64gb_black_0_3.webp",
+            text: "Смартфон Apple iPhone 11 64Gb Black",
+            price: 22999,
+        },
+        {
+            img: "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/ua-galaxy-a53-5g-a536-sm-a536ezkhsek-531826029_1__1.jpg",
+            text: "Смартфон Samsung Galaxy A53 128Gb Black",
+            price: 15999,
+        },
+        {
+            img: "/img/apple_iphone_11_64gb_black_0_3.webp",
+            text: "Смартфон Apple iPhone 11 64Gb Black",
+            price: 22999,
+        },
+        {
+            img: "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/ua-galaxy-a53-5g-a536-sm-a536ezkhsek-531826029_1__1.jpg",
+            text: "Смартфон Samsung Galaxy A53 128Gb Black",
+            price: 15999,
+        },
+        {
+            img: "/img/apple_iphone_11_64gb_black_0_3.webp",
+            text: "Смартфон Apple iPhone 11 64Gb Black",
+            price: 22999,
+        },
+        {
+            img: "https://cdn.comfy.ua/media/catalog/product/cache/5/small_image/270x265/62defc7f46f3fbfc8afcd112227d1181/u/a/ua-galaxy-a53-5g-a536-sm-a536ezkhsek-531826029_1__1.jpg",
+            text: "Смартфон Samsung Galaxy A53 128Gb Black",
+            price: 15999,
+        },
+    ],
+};
