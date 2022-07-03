e92c81d0759f656b59f7cbcd540b77f461c68392
Vlad Volkov <from5jforever@gmail.com>
2022-07-03 10:03:48 +0300

    Delete test collection
    - Delete model
    - Delete controller
    - Delete route

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,6 @@ import cors from 'cors';
 
 import { notFound } from './middleware/notFound';
 
-import testRouter from './routes/test';
 import categoryRouter from './routes/category';
 import productRouter from './routes/product';
 
@@ -18,7 +17,6 @@ const PORT = 5000;
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/v1/test', testRouter);
 app.use('/api/v1/category', categoryRouter);
 app.use('/api/v1/product', productRouter);
 
--- server/routes/test.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getTest, getAllTest, createTest, updateTest, deleteTest } from '../controllers/test';
-
-const router = express.Router();
-
-router.route('/').get(getAllTest).post(createTest);
-router.route('/:id').get(getTest).patch(updateTest).delete(deleteTest);
-
-export default router;
