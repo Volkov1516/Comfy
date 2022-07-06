f0849ae0f751813ebe2a616da88ee183c04388fa
Vlad Volkov <from5jforever@gmail.com>
2022-07-06 16:36:22 +0300

    Refactro Product component
    - Provide correct data
    - Replace map for destructured values

--- client/components/Product.tsx
+++ client/components/Product.tsx
@@ -9,6 +9,7 @@ import Popup from './common/Popup';
 import { mockCarouselData } from '../mocks/mockCarouselData';
 
 const Product = ({ product }: any) => {
+    let { name, brand, model, color, battery, colorAvailable, displayFrashrate, displayResolution, displaySize, displayType, images, labels, os, price, processor, ram, rom, rate, romAvailable } = product[0];
     let npDisplayTimeout: ReturnType<typeof setTimeout>;
     let upDisplayTimeout: ReturnType<typeof setTimeout>;
 
@@ -45,132 +46,126 @@ const Product = ({ product }: any) => {
                 <a className={css.tabItem} href="#">АКСЕССУАРЫ</a>
                 <a className={css.tabItem} href="#">СЕРВИСЫ</a>
             </div>
-            {product?.map((item: any) => (
-                <div className={css.general}>
-                    <div className={css.gallery}>
-                        <div className={css.list}>
-                            <img className={css.listItem} src="/img/apple_iphone_11_64gb_black_0_3.webp" alt="Arrow" width="52px" height="52px" />
-                            <img className={css.listItem} src="/img/apple_iphone_11_64gb_black_0_3.webp" alt="Arrow" width="52px" height="52px" />
-                            <img className={css.listItem} src="/img/apple_iphone_11_64gb_black_0_3.webp" alt="Arrow" width="52px" height="52px" />
-                            <img className={css.listItem} src="/img/apple_iphone_11_64gb_black_0_3.webp" alt="Arrow" width="52px" height="52px" />
-                            <img className={css.listItem} src="/img/apple_iphone_11_64gb_black_0_3.webp" alt="Arrow" width="52px" height="52px" />
-                        </div>
-                        <div className={css.mainImage}>
-                            <img className={css.listItem} src={item.img} alt="Arrow" width="100%" height="380px" />
-                        </div>
+            <div className={css.general}>
+                <div className={css.gallery}>
+                    <div className={css.list}>
+                        {images.map((img: string) => (
+                            <img className={css.listItem} src={img} alt="product images" width="52px" height="52px" />
+                        ))}
                     </div>
-                    <div className={css.info}>
-                        <div className={css.infoMain}>
-                            <h1 className={css.name}>Смартфон {item.title} 128Gb Black</h1>
-                            <div className={css.block}>
-                                <div className={css.labels}>
-                                    <div className={css.label} onMouseEnter={handleNpDisplayMouseEnter} onMouseLeave={handleNpDisplayMouseLeave}>
-                                        <img className={css.labelImg} src="https://cdn.comfy.ua/media/label/Group%20965%20(19)_1.svg" alt="Arrow" width="24px" height="24px" />
-                                        <span>Доставка Новою Поштою</span>
-                                        <Popup type="normal" display={npDisplay} title="Доставка Новой Почтой" text="От 99$ к отделению" button="Подробнее" />
-                                    </div>
-                                    <div className={css.label} onMouseEnter={handleUpDisplayMouseEnter} onMouseLeave={handleUpDisplayMouseLeave}>
-                                        <img className={css.labelImg} src="https://cdn.comfy.ua/media/label/Group%20967_1.svg" alt="Arrow" width="24px" height="24px" />
-                                        <span>Доставка Укрпоштою</span>
-                                        <Popup type="normal" display={upDisplay} title="Доставка Укрпочтой" text="От 50$ к отделению" button="Подробнее" />
-                                    </div>
-                                </div>
-                                <div className={css.reviews}>
-                                    rating stars, reviews and product code
-                                </div>
-                            </div>
-                        </div>
-                        <div className={css.infoBuy}>
-                            <div className={css.block}>
-                                <div className={css.price}>
-                                    <b>{item.price}</b>
-                                </div>
-                                <div className={css.buttons}>
-                                    <button className={css.buy}>Купити</button>
-                                    <button className={css.order}>Швидке замовлення</button>
-                                </div>
-                                <div className={css.controlls}>
-                                    <div className={css.img}>
-                                        <img src="/img/compare.svg" alt="Arrow" width="20px" height="20px" />
-                                    </div>
-                                    <div className={css.img}>
-                                        <img src="/img/heart.svg" alt="Arrow" width="20px" height="20px" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={css.bonus}>
-                                bonus
-                            </div>
-                        </div>
-                        <div className={css.infoCredit}>
-                            <div className={css.content}>
-                                <div className={css.item}>
-                                    <img src="https://cdn.comfy.ua/media/label/Group%20473_3.svg" alt="Arrow" />
-                                </div>
-                                <div className={css.item}>
-                                    <img src="https://cdn.comfy.ua/media/label/Group%20496_2.svg" alt="Arrow" />
-                                </div>
-                                <div className={css.item}>
-                                    <img src="https://cdn.comfy.ua/media/label/Group%20378_2.svg" alt="Arrow" />
-                                </div>
-                            </div>
-                            <button className={css.btn}>Купити в кредит</button>
-                        </div>
-                        <div className={css.infoAlternatives}>
-                            <p className={css.title}>Iншi моделi</p>
-                            <div className={css.actions}>
-                                <div className={css.colors}>
-                                    <div className={css.color}></div>
-                                </div>
-                                <div className={css.memory}>
-                                    <div className={css.item}>
-                                        {item.ram} ГБ
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={css.infoService}>
-                            <Carousel display="small" header="Выберите сервис" items={mockCarouselData.items} />
-                        </div>
-                        <div className={css.infoAccessories}>
-                            <Carousel display="small" header="Выберите аксесуар" items={mockCarouselData.items} />
-                        </div>
+                    <div className={css.mainImage}>
+                        <img className={css.listItem} src={images[0]} alt="main image" width="100%" height="380px" />
                     </div>
                 </div>
-            ))}
+                <div className={css.info}>
+                    <div className={css.infoMain}>
+                        <h1 className={css.name}>Смартфон {name} {rom}Gb {color}</h1>
+                        <div className={css.block}>
+                            <div className={css.labels}>
+                                <div className={css.label} onMouseEnter={handleNpDisplayMouseEnter} onMouseLeave={handleNpDisplayMouseLeave}>
+                                    <img className={css.labelImg} src="https://cdn.comfy.ua/media/label/Group%20965%20(19)_1.svg" alt="Arrow" width="24px" height="24px" />
+                                    <span>Доставка Новою Поштою</span>
+                                    <Popup type="normal" display={npDisplay} title="Доставка Новой Почтой" text="От 99$ к отделению" button="Подробнее" />
+                                </div>
+                                <div className={css.label} onMouseEnter={handleUpDisplayMouseEnter} onMouseLeave={handleUpDisplayMouseLeave}>
+                                    <img className={css.labelImg} src="https://cdn.comfy.ua/media/label/Group%20967_1.svg" alt="Arrow" width="24px" height="24px" />
+                                    <span>Доставка Укрпоштою</span>
+                                    <Popup type="normal" display={upDisplay} title="Доставка Укрпочтой" text="От 50$ к отделению" button="Подробнее" />
+                                </div>
+                            </div>
+                            <div className={css.reviews}>
+                                {rate} reviews and product code
+                            </div>
+                        </div>
+                    </div>
+                    <div className={css.infoBuy}>
+                        <div className={css.block}>
+                            <div className={css.price}>
+                                <b>{price}</b>
+                            </div>
+                            <div className={css.buttons}>
+                                <button className={css.buy}>Купити</button>
+                                <button className={css.order}>Швидке замовлення</button>
+                            </div>
+                            <div className={css.controlls}>
+                                <div className={css.img}>
+                                    <img src="/img/compare.svg" alt="Arrow" width="20px" height="20px" />
+                                </div>
+                                <div className={css.img}>
+                                    <img src="/img/heart.svg" alt="Arrow" width="20px" height="20px" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={css.bonus}>
+                            bonus
+                        </div>
+                    </div>
+                    <div className={css.infoCredit}>
+                        <div className={css.content}>
+                            <div className={css.item}>
+                                <img src="https://cdn.comfy.ua/media/label/Group%20473_3.svg" alt="Arrow" />
+                            </div>
+                            <div className={css.item}>
+                                <img src="https://cdn.comfy.ua/media/label/Group%20496_2.svg" alt="Arrow" />
+                            </div>
+                            <div className={css.item}>
+                                <img src="https://cdn.comfy.ua/media/label/Group%20378_2.svg" alt="Arrow" />
+                            </div>
+                        </div>
+                        <button className={css.btn}>Купити в кредит</button>
+                    </div>
+                    <div className={css.infoAlternatives}>
+                        <p className={css.title}>Iншi моделi</p>
+                        <div className={css.actions}>
+                            <div className={css.colors}>
+                                <div className={css.color}></div>
+                            </div>
+                            <div className={css.memory}>
+                                <div className={css.item}>
+                                    {ram} ГБ
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={css.infoService}>
+                        <Carousel display="small" header="Выберите сервис" items={mockCarouselData.items} />
+                    </div>
+                    <div className={css.infoAccessories}>
+                        <Carousel display="small" header="Выберите аксесуар" items={mockCarouselData.items} />
+                    </div>
+                </div>
+            </div>
             <Carousel display="normal" header="Посмотрите ещё" items={mockCarouselData.items} />
             <div className={css.features}>
-                {product.map((item: any) => (
-                    <>
-                        <h2 className={css.title}>Характеристики {item.title}</h2>
-                        <ul className={css.featureList}>
-                            <li className={cx(css.featureItem, css.withBg)}>
-                                <span className={css.featureName}>Диагональ дисплея</span>
-                                <b>{item.displaySize}</b>
-                                <img src="https://www.svgrepo.com/show/195812/question.svg" alt="Question icon" width="20px" height="20px" />
-                            </li>
-                            <li className={css.featureItem}>
-                                <span className={css.featureName}>Разрешение экрана</span>
-                                <b>{item.displayResolution}</b>
-                                <img src="https://www.svgrepo.com/show/195812/question.svg" alt="Question icon" width="20px" height="20px" />
-                            </li>
-                            <li className={cx(css.featureItem, css.withBg)}>
-                                <span className={css.featureName}>Тип экрана</span>
-                                <b>{item.displayType}</b>
-                                <img src="https://www.svgrepo.com/show/195812/question.svg" alt="Question icon" width="20px" height="20px" />
-                            </li>
-                            <li className={css.featureItem}>
-                                <span className={css.featureName}>Частота обновления экрана</span>
-                                <b>{item.frashrate} Гц</b>
-                                <img src="https://www.svgrepo.com/show/195812/question.svg" alt="Question icon" width="20px" height="20px" />
-                            </li>
-                        </ul>
-                        <button className={css.showMoreBtn}>
-                            <span>Показать больше</span>
-                            <img src="https://www.svgrepo.com/show/17594/plus.svg" alt="Question icon" width="16px" height="16px" />
-                        </button>
-                    </>
-                ))}
+                <>
+                    <h2 className={css.title}>Характеристики {name}</h2>
+                    <ul className={css.featureList}>
+                        <li className={cx(css.featureItem, css.withBg)}>
+                            <span className={css.featureName}>Диагональ дисплея</span>
+                            <b>{displaySize}</b>
+                            <img src="https://www.svgrepo.com/show/195812/question.svg" alt="Question icon" width="20px" height="20px" />
+                        </li>
+                        <li className={css.featureItem}>
+                            <span className={css.featureName}>Разрешение экрана</span>
+                            <b>{displayResolution}</b>
+                            <img src="https://www.svgrepo.com/show/195812/question.svg" alt="Question icon" width="20px" height="20px" />
+                        </li>
+                        <li className={cx(css.featureItem, css.withBg)}>
+                            <span className={css.featureName}>Тип экрана</span>
+                            <b>{displayType}</b>
+                            <img src="https://www.svgrepo.com/show/195812/question.svg" alt="Question icon" width="20px" height="20px" />
+                        </li>
+                        <li className={css.featureItem}>
+                            <span className={css.featureName}>Частота обновления экрана</span>
+                            <b>{displayFrashrate} Гц</b>
+                            <img src="https://www.svgrepo.com/show/195812/question.svg" alt="Question icon" width="20px" height="20px" />
+                        </li>
+                    </ul>
+                    <button className={css.showMoreBtn}>
+                        <span>Показать больше</span>
+                        <img src="https://www.svgrepo.com/show/17594/plus.svg" alt="Question icon" width="16px" height="16px" />
+                    </button>
+                </>
             </div>
             <div className={css.description}>
                 <h2 className={css.title}>Описание смартфона</h2>
