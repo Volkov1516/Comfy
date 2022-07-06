80e5e9f27c434f1585c7e7cd6e81889e2858a767
Vlad Volkov <from5jforever@gmail.com>
2022-07-06 16:43:52 +0300

    Refactor Category component
    - Provide correct image data

--- client/components/Category.tsx
+++ client/components/Category.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 import css from '../styles/Category.module.scss';
 
 const Category = ({ category }: any) => {
+    console.log(category)
     return (
         <div className={css.container}>
             <div>Breadcrumbs</div>
             <div className={css.category}>
                 {category?.data?.subcategories?.map((item: any) => (
                     <div className={css.categoryItem}>
-                        {/* <Link href={`/products/categoryId=${category.data._id}&model=iphone_12`}> */}
                         <Link href={`/products/categoryId=${category.data._id}`}>
                             <a>
-                                <img src="/img/apple4.webp" alt="Logo" width="80px" height="80px" />
+                                <img src={item.img} alt="Logo" width="80px" height="80px" />
                                 <h2>{item?.title}</h2>
                             </a>
                         </Link>
