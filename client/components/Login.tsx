ce18e17a6bc655134c4f27f946cd28013a8b0d2a
Vlad Volkov <from5jforever@gmail.com>
2022-07-18 20:21:52 +0300

    Refactor Login component

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -1,15 +1,19 @@
-import { useState } from 'react';
+import { useState, MouseEventHandler } from 'react';
 import axios from 'axios';
 
 import css from '../styles/Login.module.scss';
 
-const Login = ({ isOpen, handleModal }: any) => {
-    const [name, setName] = useState('');
+type LoginTypes = {
+    isOpen: boolean,
+    handleModal: MouseEventHandler<HTMLImageElement>,
+};
+
+const Login = ({ isOpen, handleModal }: LoginTypes) => {
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [user, setUser] = useState(null);
 
     const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setName(e.target.value);
+        setEmail(e.target.value);
     }
 
     const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,16 +21,14 @@ const Login = ({ isOpen, handleModal }: any) => {
     }
 
     const handleSignIn = async () => {
-        const response = await axios.post('http://localhost:5000/api/v1/auth/login', { email: name, password: password });
-        setUser(response.data);
+        const response = await axios.post('http://localhost:5000/api/v1/auth/login', { email: email, password: password });
         localStorage.setItem("token", response.data.token);
         localStorage.setItem("userEmail", response.data.user.email);
         localStorage.setItem("userId", response.data.user.id);
     };
 
     const handleSignUp = async () => {
-        const response = await axios.post('http://localhost:5000/api/v1/auth/register', { email: name, password: password });
-        setUser(response.data);
+        const response = await axios.post('http://localhost:5000/api/v1/auth/register', { email: email, password: password });
         localStorage.setItem("token", response.data.token);
         localStorage.setItem("userEmail", response.data.user.email);
         localStorage.setItem("userId", response.data.user.id);
@@ -43,10 +45,10 @@ const Login = ({ isOpen, handleModal }: any) => {
                 <div className={css.form}>
                     <img onClick={handleModal} className={css.close} src="https://www.svgrepo.com/show/66823/close.svg" alt="Close" width="14px" height="14px" />
                     <h3 className={css.header}>Вход</h3>
-                    <label className={css.label} htmlFor="name">Имя</label>
-                    <input className={css.input} value={name} onChange={handleName} type="text" id="name" />
-                    <label className={css.label} htmlFor="email">Пароль</label>
-                    <input className={css.input} value={password} onChange={handlePassword} type="password" id="email" />
+                    <label className={css.label} htmlFor="email">Имя</label>
+                    <input className={css.input} value={email} onChange={handleName} type="text" id="email" />
+                    <label className={css.label} htmlFor="password">Пароль</label>
+                    <input className={css.input} value={password} onChange={handlePassword} type="password" id="password" />
                     <button className={css.button} onClick={handleSignIn}>Войти</button>
                     <button className={css.button} onClick={handleSignUp}>Зарегистрироваться</button>
                 </div>
